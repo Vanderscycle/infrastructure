5b76c8653bd3c16f402a8dd382e85ac8e33fb3d7
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2022-11-13 14:03:09 -0800

    Kept only ready-ish services

--- charts/consul/tls/mv-certificate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl -n vault create secret tls tls-ca \
- --cert ./ca.pem  \
- --key ./ca-key.pem
-
-kubectl -n vault create secret tls tls-server \
-  --cert ./vault.pem \
-  --key ./vault-key.pem
