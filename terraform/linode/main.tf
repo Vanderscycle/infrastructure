6e84309a269d747782e82c22225e109c5755c827
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2023-05-29 19:39:09 -0700

    working locally

--- terraform/linode/main.tf
+++ terraform/linode/main.tf
@@ -1,8 +1,19 @@
-
-module "dns" {
-  source         = "./modules/dns"
+resource "linode_instance" "terraform-web" {
+  image     = "linode/ubuntu18.04"
+  label     = "Terraform-Web-Example"
+  group     = "Terraform"
+  region    = "us-east"
+  type      = "g6-standard-1"
+  root_pass = "YOUR_ROOT_PASSWORD"
 }
+# module "dns" {
+#   source         = "./modules/dns"
+# }
 
-module "lke" {
-  source         = "./modules/lke"
-}
+# module "lke" {
+#   source         = "./modules/lke"
+# }
+
+# module "bucket" {
+#   source         = "./modules/bucket"
+# }
