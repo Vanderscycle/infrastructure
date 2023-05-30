07dbe6fbe193652483781c9c34c0c21680eecafd
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2023-05-29 19:41:56 -0700

    now for real

--- terraform/linode/main.tf
+++ terraform/linode/main.tf
@@ -1,19 +1,19 @@
-resource "linode_instance" "terraform-web" {
-  image     = "linode/ubuntu18.04"
-  label     = "Terraform-Web-Example"
-  group     = "Terraform"
-  region    = "us-east"
-  type      = "g6-standard-1"
-  root_pass = "YOUR_ROOT_PASSWORD"
+# resource "linode_instance" "terraform-web" {
+#   image     = "linode/ubuntu18.04"
+#   label     = "Terraform-Web-Example"
+#   group     = "Terraform"
+#   region    = "us-east"
+#   type      = "g6-standard-1"
+#   root_pass = "YOUR_ROOT_PASSWORD"
+# }
+module "dns" {
+  source         = "./modules/dns"
 }
-# module "dns" {
-#   source         = "./modules/dns"
-# }
 
-# module "lke" {
-#   source         = "./modules/lke"
-# }
+module "lke" {
+  source         = "./modules/lke"
+}
 
-# module "bucket" {
-#   source         = "./modules/bucket"
-# }
+module "bucket" {
+  source         = "./modules/bucket"
+}
