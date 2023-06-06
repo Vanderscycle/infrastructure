bb076ec7d88b3c468138c92d8277b547d7817422
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2023-06-06 16:30:08 -0700

    made procress with terraform

---
+++ terraform/aws/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  description = "AWS region for all resources."
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "access_key" {
+  description = "IAM user access key id"
+  type    = string
+}
+
+variable "secret_key" {
+  description = "IAM user access key secret"
+  type    = string
+}
