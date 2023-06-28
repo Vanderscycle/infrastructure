3c17180a9a4c3bcb4a2d0b60e30b6e887a747dba
vanderscycle <henri-vandersleyen@protonmail.com>
2023-06-28 12:10:29 -0700

    test

--- localhost/home_network/flake.nix
+++ localhost/home_network/flake.nix
@@ -1,5 +1,19 @@
-{
-  outputs = { self }: {
+{ 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux"; 
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    # using application function
+    packages.${system}.default = pkgs.poetry2nix.mkPoetryApplication {
+      projectDir = self;
+    };
+    # nix eval .#foo
     foo = "bro";
   };
 }
