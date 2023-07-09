94c2f88f39d95ea05ff5bdd45f756254565238d3
vanderscycle <henri-vandersleyen@protonmail.com>
2023-07-09 12:38:02 -0700

    small cleanup

--- localhost/go2nix/README.md
+++ localhost/go2nix/README.md
@@ -1,4 +1,14 @@
+# NixOS learning 
+
+## Go 
 nix flake init -t github:nix-community/gomod2nix#app
+
+## Python
+Package management [Poetry](https://python-poetry.org/docs/) and how it does [it](https://toraritte.github.io/poetry-intro/#using-a-hrefhttpspython-poetryorg-titlethe-poetry-websitepoetrya-with-a-hrefhttpsnixosorg-titlethe-website-of-the-nix-cross-platform-system-package-managernixa)
+
+Nix shell [tutorial](https://nixos.org/manual/nixpkgs/stable/#python)
 nix flake init -t github:nix-community/poetry2nix#app
+
+## TS/JS
 nix flake init -t github:nix-community/dream2nix#simple
 nix flake init // (hello world)
--- localhost/home_network/home_network/main.py
+++ localhost/home_network/home_network/main.py
@@ -2,17 +2,24 @@ from diagrams import Cluster, Diagram
 from diagrams.generic.network import Switch, Router, Firewall
 from diagrams.generic.place import Datacenter
 
+# test
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+
 
 def main():
     with Diagram("Home network", show=False, direction="TB"):
         entrypoint = Router("modem")
-        with Cluster("Localhost"):
-            internals = Router("Linksys00356") >> Switch("24 poe network switch")
+        # with Cluster("Localhost"):
+        #     internals = Router("Linksys00356") >> Switch("24 poe network switch")
 
 
 def test():
-    print("hello world")
+    with Diagram("Web Service", show=False):
+        ELB("lb") >> EC2("web") >> RDS("userdb")
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    print("hello there")
